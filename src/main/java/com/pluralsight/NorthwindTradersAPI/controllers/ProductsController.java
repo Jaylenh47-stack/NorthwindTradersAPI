957eaee15c160e86a882831b514d01d999cde9f6
Jaylenh47-stack <jaylenh47@gmail.com>
2025-12-09 16:02:09 -0500

    working on ProductsController

--- src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/ProductsController.java
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/ProductsController.java
@@ -1,6 +1,8 @@
 package com.pluralsight.NorthwindTradersAPI.controllers;
 
+import com.pluralsight.NorthwindTradersAPI.dao.ProductDao;
 import com.pluralsight.NorthwindTradersAPI.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,28 +13,22 @@ import java.util.List;
 
 @RestController
 public class ProductsController {
-    private List<Product> products;
+    private ProductDao productDao;
 
-    public ProductsController(){
-        this.products = new ArrayList<>();
-        products.add(new Product(1, "Apple", 1, 1.99f));
-        products.add(new Product(2, "Blueberries", 1, 2.99f));
-        products.add(new Product(3, "Banana", 1, 0.99f));
-        products.add(new Product(4, "Beef", 2, 9.99f));
+    @Autowired
+    public ProductsController(ProductDao productDao){
+        this.productDao = productDao;
     }
 
     @RequestMapping(path = "/products", method = RequestMethod.GET)
     public List<Product> getProducts() {
-        return products;
+
+       return null;
     }
 
     @RequestMapping(path = "/products/{id}", method =RequestMethod.GET)
     public Product getProduct(@PathVariable int id){
-        for (Product p : this.products){
-            if (p.getProductId() == id){
-                return p;
-            }
-        }
+       
         return null;
     }
 
